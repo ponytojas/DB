67cf3c2f9a91c2fec77eaff3aff84cd51d77b706
Daniel <daniel.villalobosdel@gmail.com>
2019-03-26 13:16:00 +0100

    Other sample

--- ejemplos.sql
+++ ejemplos.sql
@@ -3,3 +3,4 @@ select nombre_musico, apellidos_musico, direccion_musico from musicos where dire
 select distinct direccion_musico from musicos;
 select * from grupos;
 select * from musicos where codigo_grupo_musico in (select codigo_grupo from grupos where genero_grupo = 'Jazz');
+select nombre_grupo, genero_grupo, codigo_grupo from grupos where codigo_grupo not in (select codigo_grupo_musico from musicos);
