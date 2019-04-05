8b53bc6ef4ad0cd6f223e68b6bc825ca8960cc38
Daniel Villalobos <daniel.villalobosdel@gmail.com>
2019-04-05 12:51:03 +0200

    More samples

--- ejemplos2.sql
+++ ejemplos2.sql
@@ -1,8 +1,10 @@
 INSERT INTO MUSICO
-VALUES ('25801802B', 'Jim', 'Magger', 'Calle Piedras', '581392', 'Valencia', 673858, 'GRUP00002');
+VALUES ('25801802B', 'Jim', 'Magger', 'Calle Piedras', '581392', 'Valencia', 673858);
+
 
 INSERT INTO INSTRUMENTO(NOMBRE_INSTRUMENTO, TIPO_INSTRUMENTO)
 VALUES('Gibson Les Paul', 'GUITARRA ELECTRICA');
+
 
 INSERT INTO INSTRUMENTO_MUSICO(DNI_MUSICO, ID_INSTRUMENTO)
 (
@@ -12,6 +14,12 @@ INSERT INTO INSTRUMENTO_MUSICO(DNI_MUSICO, ID_INSTRUMENTO)
 	select * from t1 cross join t2
 );
 
+
+INSERT INTO COMPOSITOR(DNI_MUSICO) VALUES ('25801802B');
+
+INSERT INTO GRUPO(GENERO_GRUPO, NOMBRE_GRUPO) VALUES ('ROCK', 'THE ROLLIN'' STONES' );
+
+
 SELECT * from musico where musico.dni_musico in 
 (
 	select instrumento_musico.dni_musico from instrumento_musico inner join 
@@ -19,4 +27,8 @@ SELECT * from musico where musico.dni_musico in
 		select * from instrumento where instrumento.tipo_instrumento = 'GUITARRA ELECTRICA'
 	) as inst
 	on instrumento_musico.id_instrumento = inst.id_instrumento
-)
+);
+
+
+SELECT DISTINCT * FROM MUSICO INNER JOIN COMPOSITOR ON COMPOSITOR.DNI_MUSICO = MUSICO.DNI_MUSICO;
+
