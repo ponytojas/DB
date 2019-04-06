246c2c05f04993c05bc369b7489d36b29379099f
Daniel Villalobos <daniel.villalobosdel@gmail.com>
2019-04-06 12:52:50 +0200

    Auto stash before merge of "v2" and "origin/v2"

--- llamadas.sql
+++ llamadas.sql
@@ -5,19 +5,3 @@ SELECT DISTINCT * FROM MUSICO INNER JOIN INSTRUMENTO_MUSICO ON INSTRUMENTO_MUSIC
 
 */
 
-
-select musicos.nombre_musico, musicos.apellidos_musico 
-from musicos 
-where codigo_instrumento_musico 
-in(
-	select instrumentos.codigo_instrumento 
-	from instrumentos 
-	where codigo_instrumento 
-	like 'INSSX%'
-) and 
-musicos.codigo_grupo_musico in 
-(
-	select codigo_grupo 
-	from grupos 
-	where genero_grupo = 'Jazz'
-)
